eeebd4c808b19dda88cba7d49180efdf926b9519
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-12-23 21:46:24 +0530

    Fixed Database Issue

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,12 +1,13 @@
 // Importing required modules
 const mongoose = require('mongoose');
 require('dotenv').config();
+const DB = 'mongodb+srv://pawarsubham438:6aO4JOECgoI7Xu71@cluster0.bemtdzm.mongodb.net/mernstack?retryWrites=true&w=majority';
 
 // Function to connect to the MongoDB database
 const connectDB = async () => {
     try {
           // Connecting to MongoDB using the provided URL
-        await mongoose.connect("mongodb+srv://praveen:praveen@praveenclustor.frwy0xs.mongodb.net/urlShortner", {
+        await mongoose.connect(DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true
            
